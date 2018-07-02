d30bb305077cbfe4256eda4a5a747cc8aec5d4bd
Ivan Seliukov <Ivan_Seliukov@epam.com>
2018-07-02 09:41:02 +0300

    add examples from Chapter 7. Modules

---
+++ modules.hs
@@ -0,0 +1,63 @@
+-- Data.List
+
+import Data.List
+import Data.Char
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Geometry
+import qualified Geometry.Sphere as Sphere  
+import qualified Geometry.Cuboid as Cuboid  
+import qualified Geometry.Cube as Cube
+  
+numUniques :: (Eq a) => [a] -> Int  
+numUniques = length . nub
+
+search :: (Eq a) => [a] -> [a] -> Bool
+search needle haystack =
+    let nlen = length needle
+    in  foldl (\acc x -> if take nlen x == needle then True else acc) False (tails haystack)
+
+-- Data.Char
+
+encode :: Int -> String -> String  
+encode shift msg = 
+    let ords = map ord msg  
+        shifted = map (+ shift) ords  
+    in  map chr shifted
+
+decode :: Int -> String -> String
+decode shift msg = encode (negate shift) msg
+
+-- Data.Map
+
+phoneBook =   
+    [("betty","555-2938")  
+    ,("bonnie","452-2928")  
+    ,("patsy","493-2928")  
+    ,("lucille","205-2928")  
+    ,("wendy","939-8282")  
+    ,("penny","853-2492")  
+    ] 
+
+findKey :: (Eq k) => k -> [(k,v)] -> Maybe v  
+findKey key = foldr (\(k,v) acc -> if key == k then Just v else acc) Nothing
+
+
+fromList' :: (Ord k) => [(k,v)] -> Map.Map k v  
+fromList' = foldr (\(k,v) acc -> Map.insert k v acc) Map.empty  
+
+phoneBook2 =   
+    [("betty","555-2938")  
+    ,("betty","342-2492")  
+    ,("bonnie","452-2928")  
+    ,("patsy","493-2928")  
+    ,("patsy","943-2929")  
+    ,("patsy","827-9162")  
+    ,("lucille","205-2928")  
+    ,("wendy","939-8282")  
+    ,("penny","853-2492")  
+    ,("penny","555-2111")  
+    ]
+
+phoneBookToMap :: (Ord k) => [(k, a)] -> Map.Map k [a]  
+phoneBookToMap xs = Map.fromListWith (++) $ map (\(k,v) -> (k,[v])) xs
